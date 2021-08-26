04dd167e5d2ad2dc9dd153002a0cb57e17e360bf
francis DAVID <contato@fdavid.com.br>
2021-08-26 08:21:42 -0300

    dotnet user-secrets init dotnet user-secrets set ConnectionStrings:DevAlias "Host=localhost;Database=mydatabase;Username=myusername;Password=mypassword"
    in scaffold use: "Name=ConnectionStrings:DevAlias"

--- Models/sensoriandoContext.cs
+++ Models/sensoriandoContext.cs
@@ -36,8 +36,7 @@ namespace sensoriando_webservice.Models
         {
             if (!optionsBuilder.IsConfigured)
             {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseNpgsql("Host=localhost;Database=sensoriando;Username=postgres;Password=masterkey");
+                optionsBuilder.UseNpgsql("Name=ConnectionStrings:DevAlias");
             }
         }
 
