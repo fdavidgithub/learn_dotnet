be735e8563a2fd0025b8e36a8589a8606d1c231f
francis DAVID <contato@fdavid.com.br>
2021-09-16 07:40:46 -0300

    Basic layout with model

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace sensoriando_webservice.Pages
+{
+
+    public class IndexModel : PageModel
+    {
+        public Boolean Auth { get; set; }
+        public String[] Sensores { get; set; }
+        public void OnGet()
+        {
+            string[] sensores = { "a", "b", "c", "d"};
+
+            Auth = false;
+            Sensores = sensores;
+        }
+    }
+}
